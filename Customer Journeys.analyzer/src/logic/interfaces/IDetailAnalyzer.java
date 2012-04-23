fb348f71984883036f59e094aefa249aa0503fd8
Florian Hagenauer <hagenauer.florian@gmail.com>
2012-04-23 14:32:14 +0200

    -> Added channel statistics to the DetailAnalyzer
    Signed-off-by: Florian Hagenauer <hagenauer.florian@gmail.com>

--- Customer Journeys.analyzer/src/logic/interfaces/IDetailAnalyzer.java
+++ Customer Journeys.analyzer/src/logic/interfaces/IDetailAnalyzer.java
@@ -1,11 +1,32 @@
 package logic.interfaces;
 
+/**
+ * This interface should be implemented by classes who offer details statistics
+ * about a customer journey. When initialized the Detail Analyzer should get a
+ * complete Customer Journey.
+ * 
+ * @author Florian Hagenauer
+ * 
+ */
 
 public interface IDetailAnalyzer {
 
-	String getDetailSetStatistic ();
-	
-	String getDetailJourneyStatistic (String ID);
-	
-	
+	/**
+	 * This method returns a String with the detailed statistics of the the
+	 * complete customer journey.
+	 * 
+	 * @return A String with statistics
+	 */
+	String getDetailSetStatistic();
+
+	/**
+	 * This method should be called if only the detailed statistics for a single
+	 * customer journey want to be seen.
+	 * 
+	 * @param ID
+	 *            The ID of the customer journey.
+	 * @return The detailed statistics of a single journey as a String.
+	 */
+	String getDetailJourneyStatistic(String ID);
+
 }
